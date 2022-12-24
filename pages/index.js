38a8fb765bd4b4dd061ef011a387fccc240c50b3
Haseeb Asif <haseebasif215@gmail.com>
2022-12-25 00:02:40 +0500

    App cleaned

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import MainBanner from "../components/HomeDemoThree/MainBanner";
-import Features from "../components/HomeDemoThree/Features";
-import Solution from "../components/HomeDemoThree/Solution";
+import MainBanner from "../components/HomeDemo/MainBanner";
+import Features from "../components/HomeDemo/Features";
+import Solution from "../components/HomeDemo/Solution";
 import PriceTableTwo from "../components/Common/PriceTableTwo";
-import WhyChooseUs from "../components/HomeDemoThree/WhyChooseUs";
-import FaqSection from "../components/HomeDemoThree/FaqSection";
+import WhyChooseUs from "../components/HomeDemo/WhyChooseUs";
+import FaqSection from "../components/HomeDemo/FaqSection";
 import Partners from "../components/Common/Partners";
 import LatestNews from "../components/Common/LatestNews";
 import Footer from "../components/_App/Footer";
 import Navbar from "../components/_App/Navbar";
 import Testimonials from "../components/Common/Testimonials";
 import TeamMember from "../components/Common/TeamMember";
-import Services from "../components/HomeDemoOne/Services";
+import Services from "../components/HomeDemo/Services";
 
 const Index = () => {
   return (
